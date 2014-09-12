c9edb0fb8fdf3063b4f973cd7f2700854377a2f8
luisxcam <luisxcam@gmail.com>
2014-09-12 18:13:20 -0400

    Used a lilbit more of Jquery. Really fun :)

--- COMP2068-Lesson02/scripts/main.js
+++ COMP2068-Lesson02/scripts/main.js
@@ -1,7 +1,12 @@
 ﻿/// <reference path="jquery.js" />
 
 if (jQuery) {
+    //target the CSS
     $("#lesson-header").show();
+
+    //Trying targeting variables
+    var paraString = $("#lesson-header p").text();
+    console.debug(paraString);
 }
 
 
